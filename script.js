acd2994750d4fafcfb3f1798c033dad4987996ac
Gildeir <gildeirlopes@gmail.com>
2021-03-11 16:14:13 -0300

    Adicionando ID's e classes para as tags

---
+++ script.js
@@ -0,0 +1,9 @@
+const input = document.getElementById('texto-tarefa');
+const criarTarefa = document.getElementById('criar-tarefa')
+const listaTarefas = document.getElementById('lista-tarefas')
+ 
+    criarTarefa.addEventListener('click', function (){
+    document.getElementById('texto-tarefa').value
+        
+    });
+
