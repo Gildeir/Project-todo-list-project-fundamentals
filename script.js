63669e2b995b5339c6391fe9e7a958cc5110179f
Gildeir <gildeirlopes@gmail.com>
2021-03-11 18:03:13 -0300

    Cria novo item ao clicar no boatao adicionar e ordena os itens por ordem de criaçao

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-const input = document.getElementById('texto-tarefa');
-const criarTarefa = document.getElementById('criar-tarefa')
-const listaTarefas = document.getElementById('lista-tarefas')
- 
-    criarTarefa.addEventListener('click', function (){
-    document.getElementById('texto-tarefa').value
-        
-    });
+let input = document.getElementById('texto-tarefa');
+const inputButton = document.getElementById('criar-tarefa')
+let listTarefas = document.querySelector('#lista-tarefas')
+let listItem = document.querySelector('#li')
+
+inputButton.addEventListener('click', function (){
+    let text = input.value
+    let li = document.createElement('li')
+    if (text === "") {
+        alert('You must wright something!')
+    }else{
+        li.innerHTML = text     
+        listTarefas.insertBefore(li,listTarefas.appendChild(li))
+        input.value = ""
+
+    }        
+});
 
