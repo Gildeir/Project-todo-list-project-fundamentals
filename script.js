8c3dee7d96afcb1b9fb93f83386d3bb9049c5bf7
Gildeir <gildeirlopes@gmail.com>
2021-03-11 21:19:18 -0300

    Adiciona text decoration line=through

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 let input = document.getElementById('texto-tarefa');
-const inputButton = document.getElementById('criar-tarefa')
+let inputButton = document.getElementById('criar-tarefa')
 let listTarefas = document.querySelector('#lista-tarefas')
 let listItem = document.querySelector('#li')
 
-inputButton.addEventListener('click', function (){
-    let text = input.value
+  inputButton.addEventListener('click', function (){
+    let userData = input.value
     let li = document.createElement('li')
-    if (text === "") {
-        alert('You must wright something!')
-    }else{
-        li.innerHTML = text     
+    
+        li.innerHTML = userData     
         listTarefas.insertBefore(li,listTarefas.appendChild(li))
-        input.value = ""
+        input.value = "";       
+  });
+  
+  listTarefas.addEventListener('click',function (evento){      
+      evento.target.classList.add('cinza')      
+  })
+  
+  listTarefas.addEventListener('dblclick',function(completed){
+    
+    if (listTarefas.style.textDecoration === 'line-through') {
+        listTarefas.style.textDecoration = 'none';
+        //listTarefas.classList.remove('completed');
+      } else {
+        listTarefas.style.textDecoration = 'line-through';
+       // listTarefas.target.classList.add ('completed');
+      }
+    });
 
-    }        
-});
+
+
+
 
